6823573dc0b35b454cb99751c464dea06952328e
hwq <1102684711@qq.com>
2020-07-27 17:08:43 +0800

    更新单元测试

--- src/juexiao-stat-sdk.ts
+++ src/juexiao-stat-sdk.ts
@@ -1,3 +1,8 @@
 import MiniStatSDK from './sdk/miniprogram'
 import BrowserStatSDK from './sdk/browser'
 export { MiniStatSDK, BrowserStatSDK }
+
+export default {
+  MiniStatSDK,
+  BrowserStatSDK
+}
--- test/juexiao-stat-sdk.test.ts
+++ test/juexiao-stat-sdk.test.ts
@@ -1,14 +1,14 @@
-import DummyClass from "../src/juexiao-stat-sdk"
+import { BrowserStatSDK } from '../src/juexiao-stat-sdk'
 
 /**
  * Dummy test
  */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
     expect(true).toBeTruthy()
   })
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  it('DummyClass is instantiable', () => {
+    expect(new BrowserStatSDK()).toBeInstanceOf(BrowserStatSDK)
   })
 })
