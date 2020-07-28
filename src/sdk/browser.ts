550f5ed49cc5f9b6a85cf0486082824540c7412f
hwq <1102684711@qq.com>
2020-07-28 13:10:11 +0800

    fix: update message

--- src/sdk/browser.ts
+++ src/sdk/browser.ts
@@ -2,7 +2,7 @@ import { UserEvent } from '../types/index'
 export default class JueXiaoBrowserStatSDK {
   options = {} as UserEvent
   init() {
-    this.options.distinct_id = 'testid'
+    this.options.distinct_id = 'testidwwwww'
     this.options.time = new Date().getTime()
     this.options.type = '_trackevent'
     console.info(this.options)
