757a0ec686da59760fc34b165cafc85b5cc9e29c
hwq <1102684711@qq.com>
2020-08-03 13:45:07 +0800

    update

--- src/juexiao-stat-sdk.ts
+++ src/juexiao-stat-sdk.ts
@@ -7,10 +7,4 @@
 
 import MiniStatSDK from './sdk/miniprogram'
 import BrowserStatSDK from './sdk/browser'
-
-// const JueXiaoStatSDK = {
-//   MiniStatSDK,
-//   BrowserStatSDK
-// }
 export { MiniStatSDK, BrowserStatSDK }
-// export default JueXiaoStatSDK
